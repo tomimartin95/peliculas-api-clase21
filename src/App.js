d76fc6b843b1dd336b12dc58a507825a07aa59e0
tomimartin95 <tomimartin95@gmail.com>
2022-11-11 21:45:37 -0300

    Paginado de api y status

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Paginado from './componentes/Paginado';
+
 
 function App() {
   return (
     <div className="App">
+      <Paginado />
 
     </div>
   );
--- src/componentes/Paginado.js
+++ src/componentes/Paginado.js
@@ -1,5 +1,35 @@
-const Paginado = () => {
 
+
+const Paginado = async () => {
+
+    try {
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/550?api_key=fa66f29448182002a076bdf6b5c6ff26&language=es-MX&page=${pagina}`)
+        console.log(respuesta);
+
+
+        if (respuesta.status === 200) {
+            const datos = await respuesta.json();
+
+            let peliculas = "";
+            datos.results.forEach(pelicula => {
+                peliculas += `
+                <div className="pelicula">
+                    <img className="poster" src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}" />
+                    <h3 className="titulo">${pelicula.title}</h3>
+                </div>
+                `;
+            });
+        } else if (respuesta.status === 401) {
+            console.log("Key incorrecta");
+        } else if (respuesta.status === 404) {
+            console.log("Lo buscado no se encuentra");
+        } else {
+            console.log("Error desconocido");
+        }
+
+    } catch (error) {
+        console.log(error.message);
+    }
 
     return (
         <div>
